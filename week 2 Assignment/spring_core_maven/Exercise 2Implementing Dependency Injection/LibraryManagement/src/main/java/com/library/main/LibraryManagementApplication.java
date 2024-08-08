80c310bb8d73cc421a3dc860a54092ba565acef4
ruppesh-dev <ruppeshsk2003@gmail.com>
2024-08-08 20:15:03 +0530

    Add files via upload

---
+++ week 2 Assignment/spring_core_maven/Exercise 2Implementing Dependency Injection/LibraryManagement/src/main/java/com/library/main/LibraryManagementApplication.java
@@ -0,0 +1,20 @@
+package com.library.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        // Load the Spring context
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Test the dependency injection
+        bookService.performService();
+    }
+}
+
---
+++ week 2 Assignment/spring_core_maven/Exercise 3 Implementing Logging with Spring AOP/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,12 @@
+
+	package com.library.repository;
+
+	public class BookRepository {
+
+	    @Override
+	    public String toString() {
+	        return "BookRepository";
+	    }
+	}
+
+
